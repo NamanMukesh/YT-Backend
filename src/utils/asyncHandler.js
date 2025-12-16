ba2c4eaf0df5780ecd5f3c66c2e54be1a2afdc30
Naman Mukesh <namanmukesh5@gmail.com>
2025-12-16 14:54:52 +0530

    Complete Setup and Code Completion in index.js (app listner)

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,19 +1,21 @@
 //M1: Promise
-// const asyncHandler = (requestHandler) => {
-//     (req, res, next) = Promise.resolve(requestHandler(req, res, next)).
-//     catch((err)=> next(err))
-// }
-
-//M2: async-await
-const asyncHandler = (requestHandler) => async (req, res, next) => {
-    try {
-        await requestHandler(req, res, next)        
-    } catch (error) {
-        res.status(err.code || 400).json({
-            success: false,
-            message: err.message
-        })
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
 
-export {asyncHandler}
+
+export { asyncHandler }
+
+//M2: async-await
+// const asyncHandler = (requestHandler) => async (req, res, next) => {
+//     try {
+//         await requestHandler(req, res, next)        
+//     } catch (error) {
+//         res.status(err.code || 400).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
