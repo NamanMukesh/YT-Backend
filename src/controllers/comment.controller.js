a722e6f1de00f27681dec730ebc353c9d0469b3b
Naman Mukesh <namanmukesh5@gmail.com>
2025-12-22 02:18:16 +0530

    Enhance comment and playlist controllers: add validation, error handling, and response messages for create, update, and delete operations

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,5 +1,6 @@
-import mongoose from "mongoose"
+import mongoose, { isValidObjectId } from "mongoose"
 import {Comment} from "../models/comment.model.js"
+import {Video} from "../models/video.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
@@ -13,14 +14,86 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {content} = req.body
+    const {videoId} = req.params
+
+    if(!content || !content.trim() || !isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid VideoID or Content is Required")
+    }
+
+    const isExist = await Video.exists({_id: videoId})
+
+    if (!isExist) {
+        throw new ApiError(404, "Video not found")
+    }
+
+    const comment = await Comment.create({
+        content: content,
+        video: videoId,
+        owner: req.user._id
+    })
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, comment, "Comment is created"))
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {content} = req.body
+    const {commentId} = req.params
+
+    if(!content || !content.trim()){
+        throw new ApiError(400, "Content can't be empty")
+    }
+
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid CommentId")
+    }
+
+    const comment = await Comment.findOneAndUpdate(
+        {
+            _id: commentId,
+            owner: req.user._id
+        },
+        {
+            $set: {
+                content: content.trim()
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    if (!comment) {
+        throw new ApiError(404, "Comment does not exists")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comment Updated"))
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params
+    
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid CommentId")
+    }
+
+    const isExist = await Comment.exists({_id: commentId})
+
+    if (!isExist) {
+        throw new ApiError(404, "Comment not exists")
+    }
+
+    const comment = await Comment.findByIdAndDelete(commentId)
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, comment, "Comment Deleted"))
 })
 
 export {
--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,26 +3,72 @@ import {Playlist} from "../models/playlist.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
+import { verify } from "jsonwebtoken"
 
 
 const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
 
     //TODO: create playlist
+    if (!name?.trim() || !description?.trim()) {
+        throw new ApiError(400, "Name or Description can't be empty")
+    }
+
+    const playlist = await Playlist.create(
+        {
+            name: name.ttrim(),
+            description: description.trim(),
+            owner: req.user._id,
+        }
+    )
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201, playlist, "Playlist is Created"))
 })
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params
     //TODO: get user playlists
+
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400, "Invalid UserId")
+    }
+
+    const playlist = await Playlist.find({owner: userId})
+
+    if (playlist.length === 0) {
+        return res
+        .status(200)
+        .json(new ApiResponse(200, [], "No playlist found for this user"))
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "User Playlist"))
 })
 
 const getPlaylistById = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     //TODO: get playlist by id
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid PlaylistId")
+    }
+
+    const playlist = await Playlist.findById(playlistId)
+
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "Playlist found"))
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
+    
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
